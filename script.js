37e9d9081a1d4d36628d3ac73cb3a00a3083db85
zohora <fatematuzzohora004@gmail.com>
2025-01-20 19:05:49 +0600

    initial commitment

---
+++ script.js
@@ -0,0 +1,6 @@
+let moblie_icon = document.getElementById("mobile_icon");
+let main_menu = document.getElementById("main_menu");
+
+moblie_icon.addEventListener("click", () => {
+    main_menu.classList.toggle('click');
+})
